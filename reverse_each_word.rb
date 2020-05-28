a68501f77680f0faeb361d98eee6759d6030d943
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-28 15:45:26 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string)
+  string_to_array = string.split(" ")
+  strings_in_array_reversed = string_to_array.collect do |element|
+    element.reverse
+  end
+  strings_in_array_reversed.join(" ")
+end
+p reverse_each_word("Hello there, and how are you?")
